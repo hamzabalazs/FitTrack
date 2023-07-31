5c15e1a5c0bbf8b856b9210976eaca503b61ec70
hamzabalazs <hamzabalazs99@gmail.com>
2023-07-31 15:54:02 +0300

    Implemented frame of profile page

---
+++ lib/models/UserProfile.dart
@@ -0,0 +1,12 @@
+class UserProfile {
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String role;
+
+  UserProfile(
+      {required this.email,
+      required this.firstName,
+      required this.lastName,
+      required this.role});
+}
--- lib/widgets/ProfileWidget.dart
+++ lib/widgets/ProfileWidget.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fittrack/models/UserProfile.dart';
 import 'package:flutter/material.dart';
 
 class ProfileWidget extends StatefulWidget {
@@ -8,11 +11,77 @@ class ProfileWidget extends StatefulWidget {
 }
 
 class _ProfileWidgetState extends State<ProfileWidget> {
+  UserProfile? currentUser;
+  int numberOfWorkouts = 0;
+
+  Future<void> _getUser() async {
+    try {
+      DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser?.uid)
+          .get();
+      if (documentSnapshot.exists) {
+        setState(() {
+          currentUser = UserProfile(
+              email: documentSnapshot['email'],
+              firstName: documentSnapshot['firstName'],
+              lastName: documentSnapshot['lastName'],
+              role: documentSnapshot['role']);
+        });
+      }
+    } catch (e) {
+      throw Exception("Error: $e");
+    }
+  }
+
+  Future<void> _getNumberOfWorkouts() async {
+    try {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('workouts')
+          .where("userId", isEqualTo: FirebaseAuth.instance.currentUser?.uid)
+          .get();
+      setState(() {
+        numberOfWorkouts = querySnapshot.docs.length;
+      });
+    } catch (e) {
+      throw Exception("Failed to fetch number of workouts");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUser();
+    _getNumberOfWorkouts();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Icon(
-      Icons.person,
-      size: 150,
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Name: ${currentUser?.firstName ?? ''} ${currentUser?.lastName ?? ''}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            Text(
+              'Email: ${currentUser?.email ?? ''}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            Text(
+              'Number of Workouts: $numberOfWorkouts',
+              style: const TextStyle(fontSize: 18),
+            ),
+            Text(
+              'Role: ${currentUser?.role ?? ''}',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
